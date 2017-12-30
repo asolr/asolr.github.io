fc463de1870ea3bcce11417039d7b72968618ff9
Asher "AC" Martin <ashermartin@gmail.com>
2017-12-30 01:59:52 -0800

    random image update

--- vidyou/vidyou.js
+++ vidyou/vidyou.js
@@ -1,3 +1,4 @@
+
 function getRandomSize(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
@@ -10,4 +11,26 @@ for (var i = 0; i < 25; i++) {
   allImages += '<img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty">';
 }
 
-$('#photos').append(allImages);
+//$('#photos').append(allImages);
+
+window.onload = choosePic;
+
+var theAd = 0;
+var adImages = new Array("pink.png","yellow.png","blue.jpg");
+
+function choosePic() {
+  theAd = Math.floor(Math.random() * adImages.length);
+  document.getElementById("advertising").src = adImages[theAd];
+
+  rotate();
+}
+
+function rotate() {
+  theAd++;
+  if (theAd == adImages.length) {
+    theAd = 0;
+  }
+  document.getElementById("advertising").src = adImages[theAd];
+
+  setTimeout(rotate, 3 * 1000);
+}
