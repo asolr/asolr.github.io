aa687186f1cfa139983dc88066098a8dfce1a60a
Asher "AC" Martin <ashermartin@gmail.com>
2017-12-31 23:37:41 -0800

    update

--- valua/controls.js
+++ valua/controls.js
@@ -10,8 +10,24 @@ function startup() {
         lng: position.coords.longitude
       };
 
+      var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">VALUA.org</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>VALUA.</b>, is a new project to evaluate and estimate the '+
+      'value or price of any product. VALUA creates several live '+
+      'Mathematical Models for the user. The user can pick or '+
+      'submit a photograph. The project prototype is confidential'+
+      'however, the source code will be in the public domain. VALUA! </p>'+
+      '<p>VALUA: <a href="http://valua.org">'+
+      'http://valua.org</a> '+
+      '(January, 2018).</p>'+
+      '</div>'+
+      '</div>';
+
       infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
+      infoWindow.setContent(contentString);
       infoWindow.open(map);
       map.setCenter(pos);
     }, function() {
